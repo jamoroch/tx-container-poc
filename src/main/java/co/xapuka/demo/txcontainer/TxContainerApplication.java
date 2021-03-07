221ce68e00e0c9589d6dd0310d6621b15afd9ef1
jamoroch <jamoroch@gmail.com>
2021-03-07 13:26:12 +0100

    init commit

---
+++ src/main/java/co/xapuka/demo/txcontainer/TxContainerApplication.java
@@ -0,0 +1,13 @@
+package co.xapuka.demo.txcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxContainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/xapuka/demo/txcontainer/TxContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.xapuka.demo.txcontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxContainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
